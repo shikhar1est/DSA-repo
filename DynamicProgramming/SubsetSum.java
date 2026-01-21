a1530d201fc3e1ff255bc7f94890f14ac6d79553
Shikhar Rai <shikharr521@gmail.com>
2026-01-21 16:38:10 +0530

    Subset Sum using Memoization

--- DynamicProgramming/SubsetSum.java
+++ DynamicProgramming/SubsetSum.java
@@ -5,12 +5,20 @@ public class SubsetSum {
         int[] arr={3, 34, 4, 12, 5, 2};
         int target=9;
         int n=arr.length;
-        System.out.println(SubsetRecursion(arr,target,n));
+        Boolean[][] dp=new Boolean[n+1][target+1];
+        System.out.println(SubsetMemoization(arr,target,n,dp));
     }
-    public static boolean SubsetRecursion(int[] arr,int target,int n){
-        if(target==0) return true;
+//    public static boolean SubsetRecursion(int[] arr,int target,int n){
+//        if(target==0) return true;
+//        if(n==0) return false;
+//        if(arr[n-1]>target) return SubsetRecursion(arr,target,n-1);
+//        return SubsetRecursion(arr,target-arr[n-1],n-1) || SubsetRecursion(arr,target,n-1);
+//    }
+    public static boolean SubsetMemoization(int[] arr,int sum,int n,Boolean[][] dp){
+        if(sum==0) return true;
         if(n==0) return false;
-        if(arr[n-1]>target) return SubsetRecursion(arr,target,n-1);
-        return SubsetRecursion(arr,target-arr[n-1],n-1) || SubsetRecursion(arr,target,n-1);
+        if(dp[n][sum]!=null) return dp[n][sum];
+        if(arr[n-1]>sum) return dp[n][sum]=SubsetMemoization(arr,sum,n-1,dp);
+        return dp[n][sum]=SubsetMemoization(arr,sum-arr[n-1],n-1,dp) || SubsetMemoization(arr,sum,n-1,dp);
     }
 }
