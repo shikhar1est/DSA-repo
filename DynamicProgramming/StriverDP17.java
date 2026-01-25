2b686f3afc05e794f0f4cdc0873233c589e3dcb7
Shikhar Rai <shikharr521@gmail.com>
2026-01-25 21:30:55 +0530

    Subset Sum equal to K

--- DynamicProgramming/StriverDP17.java
+++
@@ -1,9 +0,0 @@
-package DynamicProgramming;
-
-//Count number of subsets with sum equal to K value
-
-public class StriverDP17 {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ DynamicProgramming/StriverDPSeventeen.java
@@ -0,0 +1,24 @@
+package DynamicProgramming;
+
+//Count number of subsets with sum equal to K value
+
+public class StriverDPSeventeen {
+    public static void main(String[] args) {
+         int[] arr={1,2,3,3};
+         int n=arr.length;
+         int k=6;
+         System.out.println(recursion(arr,k,n));
+
+    }
+    public static int recursion(int[] arr,int k,int n){
+        if(n==0){
+            return k==0?1:0;
+        }
+        int pick=0;
+        if(arr[n-1]<=k){
+            pick=recursion(arr,k-arr[n-1],n-1);
+        }
+        int notpick=recursion(arr,k,n-1);
+        return pick+notpick;
+    }
+}
