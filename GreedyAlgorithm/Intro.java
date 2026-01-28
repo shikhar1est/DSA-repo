c62e2e5a671bf2119c34ce035d08953dd0fa8e01
Shikhar Rai <shikharr521@gmail.com>
2026-01-28 19:57:31 +0530

    Greedy Algorithm Intro

---
+++ GreedyAlgorithm/Intro.java
@@ -0,0 +1,21 @@
+package GreedyAlgorithm;
+
+public class Intro {
+    //Greedy is an algorithmic paradigm that builds up a solution piece by piece,
+    // always choosing the next piece that offers the most
+    // obvious and immediate benefit. Greedy algorithms are used for optimization problems.
+
+    //An optimization problem can be solved using Greedy if the problem has the following property:
+    //
+    //At every step, we can make a choice that looks best at the moment, and we get the optimal solution to the complete problem.
+    //Some popular Greedy Algorithms are Fractional Knapsack, Dijkstra’s algorithm, Kruskal’s algorithm, Huffman coding and Prim’s Algorithm
+    //Also used to get an approximation for Hard optimization problems. For example,
+    // the Traveling Salesman Problem is an NP-Hard problem.
+    // A Greedy choice for this problem is to pick the nearest unvisited city from the current city at every step.
+    // These solutions don't always produce the best optimal solution but can be used to get an approximately optimal solution.
+    //However, it's important to note that not all problems are suitable for greedy algorithms.
+    // They work best when the problem exhibits the following properties:
+    //
+    //Greedy Choice Property: The optimal solution can be constructed by making the best local choice at each step.
+    //Optimal Substructure: The optimal solution to the problem contains the optimal solutions to its sub-problems.
+}
