2b54a2e0bc1858f7e1185e942397d1434898609e
Shikhar Rai <shikharr521@gmail.com>
2026-02-02 14:19:17 +0530

    Longest Common Subsequence using Tabulation

--- DynamicProgramming/LongestCommonSubsequence.java
+++ DynamicProgramming/LongestCommonSubsequence.java
@@ -1,4 +1,5 @@
 package DynamicProgramming;
+import java.util.*;
 
 public class LongestCommonSubsequence {
     public static void main(String[] args) {
@@ -6,15 +7,33 @@ public class LongestCommonSubsequence {
         String b="acwlxy";
         int n=a.length();
         int m=b.length();
-        int ans=helper(a,b,n,m);
-        System.out.println(ans);
-    }
-    public static int helper(String a,String b,int n,int m){
-        if(n==0 || m==0) return 0;
-        if(a.charAt(n-1)==b.charAt(m-1)){
-            return 1+helper(a,b,n-1,m-1);
-        }else {
-            return Math.max(helper(a, b, n - 1, m), helper(a, b, n, m - 1));
+        int[][] dp=new int[n+1][m+1];
+        for(int[] arr : dp){
+            Arrays.fill(arr,-1);
         }
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<m+1;j++){
+                if(i==0 || j==0) dp[i][j]=0;
+            }
+        }
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(a.charAt(i-1)==b.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        System.out.println(dp[n][m]);
     }
+//    public static int helper(String a,String b,int n,int m,int[][] dp){
+//        if(n==0 || m==0) return 0;
+//        if(dp[n][m]!=-1) return dp[n][m];
+//        if(a.charAt(n-1)==b.charAt(m-1)){
+//            return dp[n][m]=1+helper(a,b,n-1,m-1,dp);
+//        }else {
+//            return dp[n][m]=Math.max(helper(a, b, n - 1, m,dp), helper(a, b, n, m - 1,dp));
+//        }
+//    }
 }
