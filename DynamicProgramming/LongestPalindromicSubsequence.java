4c098c92d8d8ede6666f1345df6efa6dde8b9a60
Shikhar Rai <shikharr521@gmail.com>
2026-02-04 20:17:40 +0530

    Longest Palindromic Subsequence

--- DynamicProgramming/LongestPalindromicSubsequence.java
+++ DynamicProgramming/LongestPalindromicSubsequence.java
@@ -1,9 +1,32 @@
 package DynamicProgramming;
 
 //DP-28
+//Just reverse the string and find the LCS of both of them
 
 public class LongestPalindromicSubsequence {
+    public static int LPS(String a,int n){
+        StringBuilder st=new StringBuilder(a);
+        String b=st.reverse().toString();
+        int[][] dp=new int[n+1][n+1];
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=n;j++){
+                if(i==0 || j==0) dp[i][j]=0;
+            }
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(a.charAt(i-1)==b.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][n];
+    }
     public static void main(String[] args) {
-
+         String a="agbcba";
+         int n=a.length();
+         System.out.println(LPS(a,n));
     }
 }
