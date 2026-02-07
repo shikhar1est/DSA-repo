aa9a8e14895af0ce9660df8a26c38e8e152e95f7
Shikhar Rai <shikharr521@gmail.com>
2026-02-07 22:52:39 +0530

    Minimum Element in a Rotated Sorted Array

---
+++ DynamicProgramming/GenerateParantheses.java
@@ -0,0 +1,15 @@
+package DynamicProgramming;
+
+//We recursively construct the sequence by considering these two conditions.
+//
+//The i-th character can be '(' if the number of opening '(' brackets used so far is less than n.
+// This ensures we don’t add more opening brackets than are needed, keeping the sequence valid.
+//The i-th character can be ')' if the number of closing ')' brackets used so far is less than the
+// number of opening '(' brackets. This ensures that each closing bracket has a matching opening
+// bracket before it, maintaining the balance.
+
+public class GenerateParantheses {
+    public static void main(String[] args) {
+
+    }
+}
