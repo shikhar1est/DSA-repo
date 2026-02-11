0ee3411affe6ff8dd460bb7373e6c0dc04d1c815
Shikhar Rai <shikharr521@gmail.com>
2026-02-11 23:51:40 +0530

    Jump Game by Recursion

--- GreedyAlgorithm/JumpGame1.java
+++ GreedyAlgorithm/JumpGame1.java
@@ -1,4 +1,32 @@
 package GreedyAlgorithm;
 
+//Naive Approach - Recursion:
+//Start from the first element and recursively call for all the elements reachable from the first element.
+// The minimum number of jumps to reach end from first can be calculated using the minimum value from
+// the recursive calls.
+//
+//minJumps(start, end) = 1 + Min(minJumps(k, end)) for all k reachable from start.
+
 public class JumpGame1 {
+    public static int minRecursion(int i,int[] arr){
+        if(i>=arr.length-1) return 0;
+        int val=Integer.MAX_VALUE;
+        for(int j=i+1;j<=i+arr[i];j++){
+            int val1=minRecursion(j,arr);
+            if(val1!=Integer.MAX_VALUE) {
+                val = Math.min(val, 1 + val1);
+            }
+        }
+        return val;
+    }
+    public static int recursion(int[] arr,int n){
+         int ans=minRecursion(0,arr);
+         if(ans==Integer.MAX_VALUE) return -1;
+         return ans;
+    }
+    public static void main(String[] args) {
+        int[] arr={1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9};
+        int n=arr.length;
+        System.out.println(recursion(arr,n));
+    }
 }
