b6b76e94a53807d683e505984f9f0e17b9cad89c
Shikhar Rai <shikharr521@gmail.com>
2026-02-24 23:52:01 +0530

    prefix sum

---
+++ RangeQueries/StaticRangeMinimumQueries.java
@@ -0,0 +1,12 @@
+package RangeQueries;
+
+//Solved using:Sparse table concept is used for fast queries on a set of static data (elements do not change).
+// It does preprocessing so that the queries can be answered efficiently.
+
+public class StaticRangeMinimumQueries {
+    public static void main(String[] args) {
+        int[] arr={7, 2, 3, 0, 5, 10, 3, 12, 18 };
+        int[][] queries={{0,4},{4,7},{7,8}};
+
+    }
+}
---
+++ RangeQueries/StaticRangeSumQueries.java
@@ -0,0 +1,9 @@
+package RangeQueries;
+
+//Solve this
+
+public class StaticRangeSumQueries {
+    public static void main(String[] args) {
+
+    }
+}
