1a2b269bc09d00ab7e648395024543895a6c1078
Shikhar Rai <shikharr521@gmail.com>
2026-03-06 21:40:48 +0530

    Rotten Oranges

--- Graphs/RottenOranges.java
+++ Graphs/RottenOranges.java
@@ -57,7 +57,7 @@ public class RottenOranges {
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==1) return -1;
-            }
+            }//kj
         }
         return time;
     }
