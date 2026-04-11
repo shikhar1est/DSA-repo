a1f241268c14ff9af89aab0d218ee2803398af3f
Shikhar Rai <shikharr521@gmail.com>
2026-04-12 01:39:17 +0530

    Stack Using An Array

---
+++ StackAndQueues/ImplementQueueUsingArray.java
@@ -0,0 +1,17 @@
+package StackAndQueues;
+
+class myQueue{
+    int[] arr;
+    int cap;
+    int top;
+    public myQueue(int n){
+        cap=n;
+
+    }
+}
+
+public class ImplementQueueUsingArray {
+    static void main() {
+
+    }
+}
